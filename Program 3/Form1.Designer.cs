6fdea3366c67de494d1aed23cdbc8e7da8b7fbdc
asbro <asbro@LAPTOP-110S6413>
2022-11-23 14:46:30 -0500

    Add project files.

---
+++ Program 3/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Program_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.farmLabel = new System.Windows.Forms.Label();
+            this.itemLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.initialCostLabel = new System.Windows.Forms.Label();
+            this.discountedCostLabel = new System.Windows.Forms.Label();
+            this.shipmentCostLabel = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.itemInputTextBox = new System.Windows.Forms.TextBox();
+            this.quantityInputTextBox = new System.Windows.Forms.TextBox();
+            this.farmComboBox = new System.Windows.Forms.ComboBox();
+            this.initialCostOutputLabel = new System.Windows.Forms.Label();
+            this.discountedCostOutputLabel = new System.Windows.Forms.Label();
+            this.shipmentCostOutputLabel = new System.Windows.Forms.Label();
+            this.totalPriceOutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // farmLabel
+            // 
+            this.farmLabel.AutoSize = true;
+            this.farmLabel.Location = new System.Drawing.Point(88, 61);
+            this.farmLabel.Name = "farmLabel";
+            this.farmLabel.Size = new System.Drawing.Size(44, 17);
+            this.farmLabel.TabIndex = 0;
+            this.farmLabel.Text = "Farm:";
+            // 
+            // itemLabel
+            // 
+            this.itemLabel.AutoSize = true;
+            this.itemLabel.Location = new System.Drawing.Point(94, 90);
+            this.itemLabel.Name = "itemLabel";
+            this.itemLabel.Size = new System.Drawing.Size(38, 17);
+            this.itemLabel.TabIndex = 1;
+            this.itemLabel.Text = "Item:";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(35, 121);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(97, 17);
+            this.quantityLabel.TabIndex = 2;
+            this.quantityLabel.Text = "Quantity (lbs):";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(119, 176);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(97, 27);
+            this.calcButton.TabIndex = 3;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // initialCostLabel
+            // 
+            this.initialCostLabel.AutoSize = true;
+            this.initialCostLabel.Location = new System.Drawing.Point(56, 236);
+            this.initialCostLabel.Name = "initialCostLabel";
+            this.initialCostLabel.Size = new System.Drawing.Size(76, 17);
+            this.initialCostLabel.TabIndex = 4;
+            this.initialCostLabel.Text = "Initial Cost:";
+            // 
+            // discountedCostLabel
+            // 
+            this.discountedCostLabel.AutoSize = true;
+            this.discountedCostLabel.Location = new System.Drawing.Point(17, 270);
+            this.discountedCostLabel.Name = "discountedCostLabel";
+            this.discountedCostLabel.Size = new System.Drawing.Size(115, 17);
+            this.discountedCostLabel.TabIndex = 5;
+            this.discountedCostLabel.Text = "Discounted Cost:";
+            // 
+            // shipmentCostLabel
+            // 
+            this.shipmentCostLabel.AutoSize = true;
+            this.shipmentCostLabel.Location = new System.Drawing.Point(29, 306);
+            this.shipmentCostLabel.Name = "shipmentCostLabel";
+            this.shipmentCostLabel.Size = new System.Drawing.Size(103, 17);
+            this.shipmentCostLabel.TabIndex = 6;
+            this.shipmentCostLabel.Text = "Shipment Cost:";
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Location = new System.Drawing.Point(52, 347);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(80, 17);
+            this.totalPriceLabel.TabIndex = 7;
+            this.totalPriceLabel.Text = "Total Price:";
+            // 
+            // itemInputTextBox
+            // 
+            this.itemInputTextBox.Location = new System.Drawing.Point(137, 88);
+            this.itemInputTextBox.Name = "itemInputTextBox";
+            this.itemInputTextBox.Size = new System.Drawing.Size(121, 22);
+            this.itemInputTextBox.TabIndex = 8;
+            // 
+            // quantityInputTextBox
+            // 
+            this.quantityInputTextBox.Location = new System.Drawing.Point(137, 118);
+            this.quantityInputTextBox.Name = "quantityInputTextBox";
+            this.quantityInputTextBox.Size = new System.Drawing.Size(121, 22);
+            this.quantityInputTextBox.TabIndex = 9;
+            // 
+            // farmComboBox
+            // 
+            this.farmComboBox.FormattingEnabled = true;
+            this.farmComboBox.Items.AddRange(new object[] {
+            "NE",
+            "NW",
+            "SE",
+            "SW"});
+            this.farmComboBox.Location = new System.Drawing.Point(137, 58);
+            this.farmComboBox.Name = "farmComboBox";
+            this.farmComboBox.Size = new System.Drawing.Size(121, 24);
+            this.farmComboBox.TabIndex = 10;
+            // 
+            // initialCostOutputLabel
+            // 
+            this.initialCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.initialCostOutputLabel.Location = new System.Drawing.Point(137, 235);
+            this.initialCostOutputLabel.Name = "initialCostOutputLabel";
+            this.initialCostOutputLabel.Size = new System.Drawing.Size(121, 23);
+            this.initialCostOutputLabel.TabIndex = 11;
+            // 
+            // discountedCostOutputLabel
+            // 
+            this.discountedCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.discountedCostOutputLabel.Location = new System.Drawing.Point(137, 269);
+            this.discountedCostOutputLabel.Name = "discountedCostOutputLabel";
+            this.discountedCostOutputLabel.Size = new System.Drawing.Size(121, 23);
+            this.discountedCostOutputLabel.TabIndex = 12;
+            // 
+            // shipmentCostOutputLabel
+            // 
+            this.shipmentCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.shipmentCostOutputLabel.Location = new System.Drawing.Point(137, 305);
+            this.shipmentCostOutputLabel.Name = "shipmentCostOutputLabel";
+            this.shipmentCostOutputLabel.Size = new System.Drawing.Size(121, 23);
+            this.shipmentCostOutputLabel.TabIndex = 13;
+            // 
+            // totalPriceOutputLabel
+            // 
+            this.totalPriceOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalPriceOutputLabel.Location = new System.Drawing.Point(138, 346);
+            this.totalPriceOutputLabel.Name = "totalPriceOutputLabel";
+            this.totalPriceOutputLabel.Size = new System.Drawing.Size(121, 23);
+            this.totalPriceOutputLabel.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 450);
+            this.Controls.Add(this.totalPriceOutputLabel);
+            this.Controls.Add(this.shipmentCostOutputLabel);
+            this.Controls.Add(this.discountedCostOutputLabel);
+            this.Controls.Add(this.initialCostOutputLabel);
+            this.Controls.Add(this.farmComboBox);
+            this.Controls.Add(this.quantityInputTextBox);
+            this.Controls.Add(this.itemInputTextBox);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.shipmentCostLabel);
+            this.Controls.Add(this.discountedCostLabel);
+            this.Controls.Add(this.initialCostLabel);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.itemLabel);
+            this.Controls.Add(this.farmLabel);
+            this.Name = "Form1";
+            this.Text = "uPickItGrocery Cost Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label farmLabel;
+        private System.Windows.Forms.Label itemLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label initialCostLabel;
+        private System.Windows.Forms.Label discountedCostLabel;
+        private System.Windows.Forms.Label shipmentCostLabel;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.TextBox itemInputTextBox;
+        private System.Windows.Forms.TextBox quantityInputTextBox;
+        private System.Windows.Forms.ComboBox farmComboBox;
+        private System.Windows.Forms.Label initialCostOutputLabel;
+        private System.Windows.Forms.Label discountedCostOutputLabel;
+        private System.Windows.Forms.Label shipmentCostOutputLabel;
+        private System.Windows.Forms.Label totalPriceOutputLabel;
+    }
+}
+
---
+++ Program 3/Form1.cs
@@ -0,0 +1,190 @@
+﻿// Grading ID: S7105
+// Program #3
+// Due date: 4/01/2021
+// CIS 199-02
+// This program calculates
+// cost of delivery for orders
+// of vegetables based on user
+// input of farm, item number
+// and quantity in pounds.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+
+
+        // declaring arrays using given values from instructions
+
+
+            string[] farm = { "NE", "NW", "SE", "SW" };     // Northeast, Northwest,
+                                                            // Southeast, Southwest farms
+
+            double[] shipmentFee = { 0.06, 0.0717, 0.07, 0.0874 };  // shipment fees for farms
+
+
+            int[] itemNumber = { 10001, 10002, 10003, 10004, 10005, 10006, 10007 };     // item numbers for veggies
+                                                                                        // (potatoes, carrots, tomatoes,
+                                                                                        //  peppers, eggplants, onions, garlic)
+
+            double[] costPerPound = { 7.87, 9.51, 10.73, 9.99, 11.99, 5.00, 4.58 };     // cost per pound for veggies
+
+
+            int[] poundLowLimits = { 0, 6, 11, 21 };        // lower limit ranges for  for discount quantity (in lbs.)
+
+            double[] discounts = { 0.00, 0.05, 0.10, 0.15 };    // % discounts for corresponding ranges
+
+
+            bool farmValid = false;         // checks if farm selection is valid
+            bool itemNumValid = false;      // checks if item number input is valid
+            bool quantityValid = false;     // checks if quantity input is valid
+            bool discountValid = false;     // checks if discount is valid
+
+            string farmSelection = "NA",    // string value for farm selection from array values
+                   farmInput = "NA";        // string value for user selection
+
+            double shipFeeSelection = 0,        // final value to correspond to shimpment fee array match
+                   costPPoundSelection = 0,     // final value to correspond to cost per pound array match
+                   discountSelection = 0,       // final value to correspond to discount array match
+
+                   initialCost,                 // total for initial cost
+                   discountedCost,              // total for discounted cost
+                   shipmentCost,                // total for shipment cost
+                   totalCost;                   // total cost using all previously gathered totals
+
+            int itemNumSelection = 0,           // final value to correspond to iterm number array match
+                itemNumInput,                   // user input item number
+                quantityInput,                  // user input quantity (in lbs.)
+                i = 0;                          // index value
+
+
+        // check for valid input values/selection:
+
+
+        // valid farm selection in combo box?
+
+           if (farmComboBox.SelectedIndex >= 0)
+           {
+               
+                farmInput = farmComboBox.Text;
+                farmValid = true;
+            }
+           else
+           {
+                MessageBox.Show("Select valid farm");
+           }
+        
+        // valid item number input?
+           
+           if (int.TryParse(itemInputTextBox.Text, out itemNumInput) 
+               && itemNumInput >= 10001 
+               && itemNumInput <= 10007)
+           {
+                itemNumValid = true;
+           }
+           else
+           {
+                MessageBox.Show("Enter a valid item number");
+           }
+
+        // valid quantity input?
+
+           if (int.TryParse(quantityInputTextBox.Text, out quantityInput) 
+                && quantityInput >= 0)
+           {
+                quantityValid = true;
+           }
+           else
+           {
+                MessageBox.Show("Enter a valid quantity");
+           }
+
+
+        // get item costs (steps 1-3):
+
+
+        // step 1: get item number and quantity for initial cost
+
+            for (i = 0; i < itemNumber.Length; i++)
+            {
+                if (itemNumValid == true && itemNumInput == itemNumber[i])
+                {
+                    itemNumSelection = itemNumber[i];
+                    costPPoundSelection = costPerPound[i];
+                }
+            }
+
+        // calculate initial cost using item number search & cost per pound
+
+            initialCost = costPPoundSelection * quantityInput;
+            initialCostOutputLabel.Text = $"{initialCost:C2}";
+
+
+        // step 2: check for discount
+
+            i = poundLowLimits.Length - 1;
+
+            while (i > 0 && !discountValid)
+            {
+                if (quantityValid && quantityInput >= poundLowLimits[i])
+                {
+                    discountValid = true;
+                }
+                else
+                {
+                    --i;
+                }
+            }
+
+            if (discountValid == true)
+            {
+                discountSelection = discounts[i];
+            }
+
+        // calculate discount using while loop above
+
+            discountedCost = initialCost - (initialCost * discountSelection);
+            discountedCostOutputLabel.Text = $"{discountedCost:C2}";
+
+        // step 3: get farm selection and shipment fee using initial cost and/or discounted cost
+
+            for (i = 0; i < farm.Length; i++)
+            {
+                
+                if (farmValid && farmInput == farm[i])
+                {
+                    farmSelection = farm[i];
+                    shipFeeSelection = shipmentFee[i];
+                }
+            }
+
+        //calculate shipment fee
+
+            shipmentCost = discountedCost * shipFeeSelection;
+            shipmentCostOutputLabel.Text = $"{shipmentCost:C2}";
+
+        // total price calculations
+
+            totalCost = discountedCost + shipmentCost;
+            totalPriceOutputLabel.Text = $"{totalCost:C2}";
+
+        }
+    }
+}
